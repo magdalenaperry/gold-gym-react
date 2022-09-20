13df3c5fecb35971b6670dbe6a561088489f4aba
Magdalena Perry <magperry16@gmail.com>
2022-09-20 11:06:37 -0400

    components created for HeroBanner, SearchExercise, Exercises, and Home page updated. The Home page hero banner was styled

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+const SearchExercises = () => {
+  return (
+    <>
+      <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+        <Typography
+          fontWeight={700}
+          mb="50px"
+          textAlign="center"
+          sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        >
+          Awesome Exercises You <br />
+          Should Know
+        </Typography>
+        <Box position="relative" mb="72px">
+          <TextField
+            sx={{
+              input: {
+                fontWeight: "700",
+                border: "none",
+                borderRadius: "4px",
+              },
+              width: { lg: "800px", xs: "350px" },
+              backgroundColor: "#FFF",
+              borderRadius: "40px",
+            }}
+            height="76px"
+            onChange={(e) => {}}
+            placeholder="Search Exercises"
+          />
+          <Button
+            className="search-btn"
+            sx={{
+              bgcolor: "#FF2625",
+              color: "#FFF",
+              textTransform: "none",
+              width: { lg: "175px", xs: "80px" },
+              fontSize: { lg: "20px", xs: "14px" },
+              height: "56px",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            Search
+          </Button>
+        </Box>
+      </Stack>
+    </>
+  );
+};
+
+export default SearchExercises;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import BroswerRouter from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BroswerRouter>
+  <BrowserRouter>
     <App />
-  </BroswerRouter>
+  </BrowserRouter>
 );
